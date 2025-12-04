6be1dcc0ea9b48b89d6467152a192097662f9857
vishnuboligili <vishnuboligili@gmail.com>
2025-12-04 15:14:05 +0530

    backend is created and also mongo db also added to it

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Mongoose Schema
+const TodoSchema = new mongoose.Schema({
+  text: { type: String, required: true }
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+// Routes
+
+// Get tasks
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+// Add task
+app.post("/todos", async (req, res) => {
+  const todo = new Todo({ text: req.body.text });
+  await todo.save();
+  res.json(todo);
+});
+
+// Delete task
+app.delete("/todos/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
