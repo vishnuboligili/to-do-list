6fbc079ae2bd47470ad8083b7fd556072a1436ef
vishnuboligili <vishnuboligili@gmail.com>
2025-12-09 22:42:38 +0530

    testing prettier using format

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,10 +1,9 @@
-export default[
-    {
-        files:['**/*.js'], //only check .js files in server side
-        rules:{
-            semi:'error',//tell any semicolons errors
-            'no-unused-vars':'warn' //warn if variables are not used
-        }
-
-    }
+export default [
+  {
+    files: ['**/*.js'], //only check .js files in server side
+    rules: {
+      semi: 'error', //tell any semicolons errors
+      'no-unused-vars': 'warn', //warn if variables are not used
+    },
+  },
 ];
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -12,35 +12,35 @@ app.use(express.json());
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
 // Mongoose Schema
 const TodoSchema = new mongoose.Schema({
-  text: { type: String, required: true }
+  text: { type: String, required: true },
 });
 
-const Todo = mongoose.model("Todo", TodoSchema);
+const Todo = mongoose.model('Todo', TodoSchema);
 
 // Routes
 
 // Get tasks
-app.get("/todos", async (req, res) => {
+app.get('/todos', async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
 // Add task
-app.post("/todos", async (req, res) => {
+app.post('/todos', async (req, res) => {
   const todo = new Todo({ text: req.body.text });
   await todo.save();
   res.json(todo);
 });
 
 // Delete task
-app.delete("/todos/:id", async (req, res) => {
+app.delete('/todos/:id', async (req, res) => {
   await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: "Task deleted" });
+  res.json({ message: 'Task deleted' });
 });
 
 const PORT = process.env.PORT || 5000;
